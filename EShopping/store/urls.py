59edbd45cc32fa8cd90350f7cb0a59e94f2b12d1
Suman <sumanrawool2@gmail.com>
2022-11-17 13:39:37 +0530

    Order functionality added

--- EShopping/store/urls.py
+++ EShopping/store/urls.py
@@ -5,6 +5,8 @@ from .views.signup import Signup
 from .views.login import Login, logout
 from .views.cart import Cart
 from .views.checkout import CheckOut
+from .views.orders import Order
+from .middlewares.auth import auth_middleware
 
 urlpatterns = [
     path('', Index.as_view(), name='homepage'),
@@ -12,5 +14,6 @@ urlpatterns = [
     path('login', Login.as_view(), name='login'),
     path('logout', logout, name='logout'),
     path('cart', Cart.as_view(), name='cart'),
-    path('check-out', CheckOut.as_view(), name='checkout')
+    path('check-out', CheckOut.as_view(), name='checkout'),
+path('orders', auth_middleware(Order.as_view()), name='orders')
 ]
