878f3b6de63a9cc4096a7ac7b856251a4e6a4f8b
Suman <sumanrawool2@gmail.com>
2022-11-26 13:37:14 +0530

    SignUp fuctionality added

--- MyCourse/courses/urls.py
+++ MyCourse/courses/urls.py
@@ -16,13 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import HttpResponse
-from courses.views import home, coursePage,signup,login
+from courses.views import home, coursePage,SignupView,login
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('', home, name='home'),
-    path('signup', signup, name='signup'),
+    path('signup', SignupView.as_view(), name='signup'),
     path('login', login, name='login'),
     path('course/<str:slug>', coursePage, name='coursepage'),
 
--- MyCourse/courses/views/auth.py
+++ MyCourse/courses/views/auth.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.shortcuts import HttpResponse
 from courses.models import Course, Video
-from django.contrib.auth.forms import UserCreationForm
+from courses.forms import RegistrationForm
+from django.views import View
 
-def signup(request):
-    if request.method =="GET":
-        form=UserCreationForm()
-        return render(request, template_name="courses/signup.html",context={'form':form})
-    return HttpResponse("POST REQUEST")
+class SignupView(View):
+    def get(self,request):
+        form = RegistrationForm()
+        return render(request, template_name="courses/signup.html", context={'form': form})
+    def post(self,request):
+        form = RegistrationForm(request.POST)
+
+        if (form.is_valid()):
+            user = form.save()
+            if (user is not None):
+                return redirect('login')
+        return render(request, template_name="courses/signup.html", context={'form': form})
+
 def login(request):
     return render(request,template_name="courses/login.html")
